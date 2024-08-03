7f540cc408dc40bc8f0f15150ebfa0389b336fc6
Karan Parashar <karan@pixelos.net>
2024-08-03 22:30:04 +0530

    sm8250-common: Build libutils.vendor
    E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.
    
    Change-Id: I5bd9623fa7a25efd5c429bac3eca78e10855219e
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>
    Signed-off-by: bxySo <bxyso@outlook.com>

--- kona.mk
+++ kona.mk
@@ -145,6 +145,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
