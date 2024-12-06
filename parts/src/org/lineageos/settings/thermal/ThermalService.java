bbe4b581f22ad81993b9b3fe9e810171c5f207cd
bxySo <bxyso@outlook.com>
2024-12-06 10:49:44 +0800

    sm8250-common: parts: Use Gaming Thermal Profiles for GameSpace's game list

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -115,10 +115,9 @@ public class ThermalService extends Service {
                 String foregroundApp = info.topActivity.getPackageName();
                 if (!foregroundApp.equals(mPreviousApp)) {
                     if (!isConfigured(foregroundApp) && isListedOnGameSpace(foregroundApp)) {
-                        mThermalUtils.setThermalProfileForce(ThermalUtils.STATE_GAMING);
-                    } else {
-                        mThermalUtils.setThermalProfile(foregroundApp);
+                        mThermalUtils.writePackage(foregroundApp, ThermalUtils.STATE_GAMING);
                     }
+                    mThermalUtils.setThermalProfile(foregroundApp);
                     mPreviousApp = foregroundApp;
                 }
             } catch (Exception e) {}
