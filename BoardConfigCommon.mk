eb76ed94b62fb48eebaaf3898894cc11eaa2537a
bxySo <bxyso@outlook.com>
2024-12-06 22:22:06 +0800

    sm8250-common: Swtich to EROFS read-only file system
    - Ref: https://source.android.com/docs/core/architecture/kernel/erofs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,14 +131,13 @@ SSI_PARTITIONS := product system system_ext
 TREBLE_PARTITIONS := odm vendor
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
-$(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4))
-
-$(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
+$(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
 
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+
+BOARD_EROFS_PCLUSTER_SIZE := 262144
 
 # Partitions - dynamic
 BOARD_SUPER_PARTITION_SIZE := 9126805504
