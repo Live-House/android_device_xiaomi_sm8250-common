2e59efbc079003e5b4ecf97fe609ced2949df952
bxySo <bxyso@outlook.com>
2024-12-06 21:24:02 +0800

    sm8250-common: parts: Set an icon for each mode of the Thermal Profiles tile

--- parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
@@ -193,7 +193,7 @@ public class ThermalSettingsFragment extends PreferenceFragment
         mSession.rebuild(mActivityFilter, ApplicationsState.ALPHA_COMPARATOR);
     }
 
-    private int getStateDrawable(int state) {
+    protected int getStateDrawable(int state) {
         switch (state) {
             case ThermalUtils.STATE_BENCHMARK:
                 return R.drawable.ic_thermal_benchmark;
