5e6a287c7769e594f197819ebbccfc5fcbc08136
bxySo <bxyso@outlook.com>
2024-12-07 10:54:24 +0800

    sm8250-common: Set EROFS Pcluster size to 64 KB
    * For devices storing a large number of small files (e.g., applications, system files, etc.),
      a smaller PCluster Size (64KB) improves storage space utilization.
      Each PCluster can map small files more finely, thus avoiding wasted space.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,7 +137,7 @@ $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
-BOARD_EROFS_PCLUSTER_SIZE := 262144
+BOARD_EROFS_PCLUSTER_SIZE := 65536
 
 # Partitions - dynamic
 BOARD_SUPER_PARTITION_SIZE := 9126805504
