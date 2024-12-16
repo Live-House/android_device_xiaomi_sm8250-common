866bede4bda020c1e15bd3abcbf9a17e52e87823
bxySo <bxyso@outlook.com>
2024-12-17 01:30:10 +0800

    sm8250-common: Drop lineage health HAL
    Revert "sm8250-common: Build lineage health HAL"
    This reverts commit 731094c73cb9c008d481d7cffc98a4e62d3f4e66.

--- kona.mk
+++ kona.mk
@@ -257,10 +257,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
