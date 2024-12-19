2261f96a896851590083a42796971df6eebc3e90
bxySo <bxyso@outlook.com>
2024-12-20 00:41:30 +0800

    sm8250-common: vintf: Move to common VINTF
    - hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
    - hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -223,6 +223,8 @@ endif
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
 ifneq ($(TARGET_IS_TABLET),true)
