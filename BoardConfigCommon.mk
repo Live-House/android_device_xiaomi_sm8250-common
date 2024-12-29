3edbb1c7dcf0f705ee27c83f3d17e0e2d363fa78
bxySo <bxyso@outlook.com>
2024-12-29 20:37:52 +0800

    Revert "sm8250-common: Set EROFS Pcluster size to 64 KB"
    It should be the same as BOARD_FLASH_BLOCK_SIZE,
    which is equivalent to (BOARD_KERNEL_PAGESIZE * 64)
    
    This reverts commit 6f53aad50e52f5abd8da9183509810e6e46a5f1f.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,7 @@ $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
-BOARD_EROFS_PCLUSTER_SIZE := 65536
+BOARD_EROFS_PCLUSTER_SIZE := 262144
 
 # Partitions - dynamic
 BOARD_SUPER_PARTITION_SIZE := 9126805504
