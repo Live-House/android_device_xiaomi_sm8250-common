5d36607fd5638b669129dfd2aee2f4f2e5c4b1fc
bxySo <bxyso@outlook.com>
2025-01-14 09:08:52 +0800

    sm8250-common: Move PocketJudge to hardware/xiaomi

--- PocketJudge/app/src/main/README.md
+++
@@ -1,3 +0,0 @@
-# Battery-friendly Pocket Mode At Kernel Level
-
-https://github.com/iusmac/battery-friendly-pocketmode
--- PocketJudge/app/src/main/gen/com/github/iusmac/pocketjudge/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-package com.github.iusmac.pocketjudge;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = false;
-    public static final String APPLICATION_ID = "com.github.iusmac.pocketjudge";
-}
