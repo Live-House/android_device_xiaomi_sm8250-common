3c580b9c71d0dca662cd661d52c68aea3da208f7
bxySo <bxyso@outlook.com>
2025-01-24 13:29:21 +0800

    Revert "sm8250-common: Move *.prop files to properties directory"
    This reverts commit 779e641f271790ddcca0c1c18807d51778ecc692.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,11 +158,11 @@ BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
 
 # Properties
-TARGET_ODM_PROP += $(COMMON_PATH)/properties/odm.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/properties/system.prop
-TARGET_VENDOR_PROP += $(COMMON_PATH)/properties/vendor.prop
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)
-TARGET_VENDOR_PROP += $(COMMON_PATH)/properties/vendor_phone.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor_phone.prop
 endif
 
 # Recovery
