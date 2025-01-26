9aac8fe222cbe0106d706180c6e529660512eca0
bxySo <bxyso@outlook.com>
2025-01-27 00:02:37 +0800

    sm8250-common: Kang power-libperfmgr from lineage
    Also removed the soong namespace in Android.bp
    
    from: https://github.com/LineageOS/android_hardware_lineage_interfaces/tree/lineage-22.1/power-libperfmgr

--- kona.mk
+++ kona.mk
@@ -318,7 +318,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
