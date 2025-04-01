6e921a174c8b616a76147f6ac1bd4161337c9019
LuK1337 <priv.luk@gmail.com>
2025-04-01 16:11:23 +0200

    sm8250-common: Add libwfdservice_shim for WFD
    e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I3badfc56581a6bf3de5d7e996fa51f59eb164aae

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,8 @@ from extract_utils.fixups_lib import (
 )
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
     (
         'vendor/lib64/libdlbdsservice.so',
         'vendor/lib/libstagefright_soft_ac4dec.so',
