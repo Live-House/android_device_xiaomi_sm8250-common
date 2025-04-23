b285488ffd25058b8da9b2278da34e8a11fa5948
bxySo <bxyso@outlook.com>
2025-04-23 21:06:08 +0800

    sm8250-common: Don't Build Qualcomm PowerHAL
    https://github.com/yaap/device_qcom_common/blob/3a3878a323e19bf63e656a38dea7b9c0b749f160/common.mk#L182

--- kona.mk
+++ kona.mk
@@ -292,6 +292,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+TARGET_PROVIDES_POWERHAL := true
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
